352846b586e06be4d69497f95665c6aeef2d4c00
Peter Larsson <peter.larsson@callistaenterprise.se>
2018-07-03 14:52:51 +0200

    BUILDENV: config prepared for openshift

---
+++ devops/openshift/test/env/secret-env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export ACTIVEMQ_RECEIVER_QUEUE_NAME=test.statistik.utlatande.queue
+export ACTIVEMQ_BROKER_USERNAME=${ACTIVEMQ_BROKER_USERNAME:-admin}
+export ACTIVEMQ_BROKER_PASSWORD=${ACTIVEMQ_BROKER_PASSWORD:-admin}
+
+export SPRING_PROFILES_ACTIVE="${SPRING_PROFILES_ACTIVE:-dev}"
+
+export CATALINA_OPTS_APPEND="\
+-Dconfig.file=/opt/$APP_NAME/config/logsender.properties \
+-Dlogback.file=/opt/$APP_NAME/config/logback.xml \
+-Dcertificate.folder=/opt/$APP_NAME/env \
+-Djava.awt.headless=true \
+-credentials.file=/opt/$APP_NAME/env/secret-env.properties \
+-Dstatistics.resources.folder=/tmp/resources \
+-Dfile.encoding=UTF-8 \
+-DbaseUrl=http://${APP_NAME}:8080"
