63836a25b77d2151d4999ca0aadf0b6ba81f61a4
Peter Larsson <peter.larsson@callistaenterprise.se>
2018-07-06 11:40:46 +0200

    INTYG-6784: Ersatt JNDI med app. konfigurerad activemq. Skapat logback konf. för OCP och lagt all logback konf. i classpath/resources

--- devops/openshift/test/env/secret-env.sh
+++ devops/openshift/test/env/secret-env.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export ACTIVEMQ_RECEIVER_QUEUE_NAME=test.statistik.utlatande.queue
 export ACTIVEMQ_BROKER_USERNAME=${ACTIVEMQ_BROKER_USERNAME:-admin}
 export ACTIVEMQ_BROKER_PASSWORD=${ACTIVEMQ_BROKER_PASSWORD:-admin}
 
@@ -8,7 +7,7 @@ export SPRING_PROFILES_ACTIVE="${SPRING_PROFILES_ACTIVE:-dev}"
 
 export CATALINA_OPTS_APPEND="\
 -Dconfig.file=/opt/$APP_NAME/config/logsender.properties \
--Dlogback.file=/opt/$APP_NAME/config/logback.xml \
+-Dlogback.file=classpath:logback-ocp.xml \
 -Dcertificate.folder=/opt/$APP_NAME/env \
 -Djava.awt.headless=true \
 -credentials.file=/opt/$APP_NAME/env/secret-env.properties \
