be906b75c71328c8f9231925cd3791549b7957bd
Erik Lupander <erik.lupander@callistaenterprise.se>
2018-10-22 11:15:15 +0200

    BUILDENV: Stage-konfig och installationsanvisning

---
+++ devops/openshift/stage/env/secret-env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export ACTIVEMQ_BROKER_USERNAME=${ACTIVEMQ_BROKER_USERNAME:-admin}
+export ACTIVEMQ_BROKER_PASSWORD=${ACTIVEMQ_BROKER_PASSWORD:-admin}
+
+export SPRING_PROFILES_ACTIVE="${SPRING_PROFILES_ACTIVE:-qa,caching-enabled,redis-sentinel}"
+
+export CATALINA_OPTS_APPEND="\
+-Dconfig.file=/opt/$APP_NAME/config/logsender.properties \
+-Dconfig.dir=/opt/$APP_NAME/config \
+-Dlogback.file=classpath:logback-ocp.xml \
+-Dcertificate.folder=/opt/$APP_NAME/certifikat \
+-Djava.awt.headless=true \
+-Dcredentials.file=/opt/$APP_NAME/env/secret-env.properties \
+-Dresources.folder=/tmp/resources \
+-Dfile.encoding=UTF-8 \
+-DbaseUrl=http://${APP_NAME}:8080"
