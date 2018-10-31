a20979374ea7297f31681d56a9da30216cc56e16
Peter Larsson <peter.larsson@callistaenterprise.se>
2018-10-31 13:41:03 +0100

    BUILDENV: openshift config updated

--- devops/openshift/test/env/secret-env.sh
+++ devops/openshift/test/env/secret-env.sh
@@ -3,14 +3,15 @@
 export ACTIVEMQ_BROKER_USERNAME=${ACTIVEMQ_BROKER_USERNAME:-admin}
 export ACTIVEMQ_BROKER_PASSWORD=${ACTIVEMQ_BROKER_PASSWORD:-admin}
 
-export SPRING_PROFILES_ACTIVE="${SPRING_PROFILES_ACTIVE:-dev}"
+export SPRING_PROFILES_ACTIVE="${SPRING_PROFILES_ACTIVE:-test,wc-all-stubs,wc-security-test,testability-api,caching-enabled}"
 
 export CATALINA_OPTS_APPEND="\
 -Dconfig.file=/opt/$APP_NAME/config/logsender.properties \
+-Dconfig.dir=/opt/$APP_NAME/config \
 -Dlogback.file=classpath:logback-ocp.xml \
--Dcertificate.folder=/opt/$APP_NAME/env \
+-Dcertificate.folder=/opt/$APP_NAME/certifikat \
 -Djava.awt.headless=true \
 -Dcredentials.file=/opt/$APP_NAME/env/secret-env.properties \
--Dstatistics.resources.folder=/tmp/resources \
+-Dresources.folder=/tmp/resources \
 -Dfile.encoding=UTF-8 \
 -DbaseUrl=http://${APP_NAME}:8080"
